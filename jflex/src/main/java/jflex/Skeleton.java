ee27ec52434d89e80f642931391856ed0705d4e4
Gregory.Shrago <gregory.shrago@jetbrains.com>
2018-03-23 16:49:44 +0300

    Original skeleton produces uncompilable code #1

--- jflex/src/main/java/jflex/Skeleton.java
+++ jflex/src/main/java/jflex/Skeleton.java
@@ -8,12 +8,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package jflex;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +29,7 @@ import java.util.List;
 public class Skeleton {
 
   /** location of default skeleton */
-  private static final String DEFAULT_LOC = "jflex/skeleton.default"; // $NON-NLS-1$
+  private static final String DEFAULT_LOC = "jflex/idea-flex.skeleton"; // $NON-NLS-1$
 
   /** expected number of sections in the skeleton file */
   private static final int size = 21;
